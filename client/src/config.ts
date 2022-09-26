3ad48a79a71e14a5448554bdd0caaa8c76a8201d
oadeniran <oadeniran82@gmail.com>
2022-09-26 04:44:43 +0100

    Initial commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'ooqhtmkbfa';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-u9evurtg.us.auth0.com',
+  clientId: 'Gpl3oLekT8EtzgyvdVMAEiJ8DqAjAnsr',
+  callbackUrl: 'http://localhost:3000/callback'
+};
